8166674448fb7f4920027e8b808f0e950d9e8e1b
Oluwaseyi Giwa <74715251+OluwaseyiWater@users.noreply.github.com>
2025-06-14 15:29:18 +0200

    Update train_maml_pytorch.py

--- train_maml_pytorch.py
+++ train_maml_pytorch.py
@@ -7,6 +7,10 @@ import os
 import pickle
 from typing import Dict, Any
 import wandb
+import warnings
+
+# Suppress gymnasium warnings about plugins
+warnings.filterwarnings("ignore", category=UserWarning, module="gymnasium.envs.registration")
 
 # Import PyTorch versions
 from mLN.environment import DynamicSpectrumEnv
@@ -45,6 +49,10 @@ def main(cfg: DictConfig) -> None:
     
     # Create environment using Hydra's instantiate
     env_config = OmegaConf.to_container(cfg.env, resolve=True)
+    
+    # Remove Hydra-specific keys
+    if '_target_' in env_config:
+        env_config.pop('_target_')
     
     # Add device to env config
     env_config['device'] = device.type
